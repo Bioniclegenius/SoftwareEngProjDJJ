a8068b58c415caa7048bba8fffebed52b4f3a355
jss87230 <jss87230@ucmo.edu>
2015-09-02 16:54:06 -0500

    Start of Project
    Here is the base folder area for the project please update here

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/************************************************************
+ * Project: Commerce Bank Project                           *
+ * Programmer: Jackson, David, Ryan                         *
+ * Date Created: 9/2/15                                     *
+ * Description: ????                                        *
+ ***********************************************************/
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
