067ab2c7670a3f6d2e4af33b80fe0640b79ff0e6
jss87230 <jss87230@ucmo.edu>
2015-10-27 11:23:42 -0500

    Minutes and Project Updates
    Minutes File updated
    
    Project updated to have start of Generate file not successful but good
    start.

--- NetBeans/Basic Commerce Bank Project/src/java/com/commerceBank/studentProject/Generate.java
+++ NetBeans/Basic Commerce Bank Project/src/java/com/commerceBank/studentProject/Generate.java
@@ -6,11 +6,14 @@
 package com.commerceBank.studentProject;
 
 import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import javax.security.auth.x500.X500Principal;
 import org.bouncycastle.operator.ContentSigner;
-import org.bouncycastle.*;
-import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 import org.bouncycastle.pkcs.PKCS10CertificationRequest;
 import org.bouncycastle.pkcs.PKCS10CertificationRequestBuilder;
+import org.bouncycastle.operator.jcajce.*;
 import org.bouncycastle.pkcs.jcajce.JcaPKCS10CertificationRequestBuilder;
 /**
  *
@@ -20,15 +23,42 @@ public class Generate {
     
     String CSR = "", PK = "";
     
-    public void generate(String country, String state, String locality, String organization, String organizationUnit, String commonName){
+    /**
+     *
+     * @param country
+     * @param state
+     * @param locality
+     * @param organization
+     * @param organizationUnit
+     * @param commonName
+     */
+    public void generate(String country, String state, String locality, String organization, String organizationUnit, String commonName) throws Exception{
+        KeyPairGenerator gen = KeyPairGenerator.getInstance("RSA");
+        gen.initialize(2048);
+        KeyPair pair = gen.generateKeyPair();
+        PrivateKey privateKey = pair.getPrivate();
+        PublicKey publicKey = pair.getPublic();
+        X500Principal subject = new X500Principal ("C=NO, ST=Trondheim, L=Trondheim, O=Senthadev, OU=Innovation, CN=www.senthadev.com, EMAILADDRESS=senthadev@gmail.com");
+        ContentSigner signGen = new JcaContentSignerBuilder("SHA1withRSA").build(privateKey);
+        PKCS10CertificationRequestBuilder builder = new JcaPKCS10CertificationRequestBuilder(subject, publicKey);
+        PKCS10CertificationRequest csr = builder.build(signGen);
+        
+        PK = privateKey.toString();
+        CSR = csr.toString();
+        
+/*Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+        X500Name x500 = new X500Name("CN=..."); //enter your DN here
+        CertificationRequestInfo csrInfo = new CertificationRequestInfo(x500, publicKeyInfo, new DERSet()); //you must instantiate publicKeyInfo beforehand
+        UnsignedPkcs10CertificationRequest pkcs10 = new UnsignedPkcs10CertificationRequest(csrInfo);*/
+
         /*KeyPair pair = generateKeyPair();
         PKCS10CertificationRequestBuilder p10Builder = new JcaPKCS10CertificationRequestBuilder(
         new X500Principal("CN=Requested Test Certificate"), pair.getPublic());
         JcaContentSignerBuilder csBuilder = new JcaContentSignerBuilder("SHA256withRSA");
         ContentSigner signer = csBuilder.build(pair.getPrivate());
         PKCS10CertificationRequest csr = p10Builder.build(signer);*/
-        PK = "abcd";
-        CSR = "1234";//csr.toString();
+        //PK = "abcd";
+        //CSR = "1234";//csr.toString();
     }
     
     public String getPrivateKey(){
