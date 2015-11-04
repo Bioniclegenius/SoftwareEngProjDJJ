1c5feb1a10f6b6d41569680cc52eb7f004659ddd
jss87230 <jss87230@ucmo.edu>
2015-11-04 17:05:19 -0600

    Minutes Update and Program Update
    Added Certificate to Private Key
    Updated Minutes File

--- NetBeans/Basic Commerce Bank Project/src/java/com/commerceBank/studentProject/CreateKeystore.java
+++ NetBeans/Basic Commerce Bank Project/src/java/com/commerceBank/studentProject/CreateKeystore.java
@@ -5,10 +5,47 @@
  */
 package com.commerceBank.studentProject;
 
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.security.cert.Certificate;
+import java.security.KeyStore;
+import java.security.cert.CertificateFactory;
+
 /**
  *
  * @author Jackson
  */
 public class CreateKeystore {
-    
+    public void makeKeystore(String cert, String pk, String ps, String alias) throws Exception{
+        KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+        // get user password and file input stream
+        char[] password = ps.toCharArray();
+
+        FileInputStream fis = null;
+        try {
+            fis = new java.io.FileInputStream("keyStoreName");
+            ks.load(fis, password);
+            
+            // Add the certificate
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            InputStream cif = new ByteArrayInputStream(cert.getBytes());
+            Certificate certs = (Certificate) cf.generateCertificate(cif);
+            ks.setCertificateEntry(alias, certs);
+            
+            // Add the Private key
+            //Check out this page https://www.nealgroothuis.name/import-a-private-key-into-a-java-keystore/
+
+            // Save the new keystore contents
+            FileOutputStream out = new FileOutputStream("name.keystore");
+            ks.store(out, password);
+            out.close();
+        } finally {
+            if (fis != null) {
+                fis.close();
+            }
+        }
+        
+    }
 }
--- NetBeans/Basic Commerce Bank Project/web/create.jsp
+++ NetBeans/Basic Commerce Bank Project/web/create.jsp
@@ -5,17 +5,30 @@
 --%>
 
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ page language="java" import="java.util.*" %>
+<jsp:useBean id="create" scope="session" class="com.commerceBank.studentProject.CreateKeystore"/>
 <!DOCTYPE html>
 <html>
+    <%
+        if(request.getParameter("certInput") != null){
+            String certificate = request.getParameter("certInput");
+            String pk = request.getParameter("pkInput");
+            String password = request.getParameter("password");
+            String alias = request.getParameter("alias");
+            create.makeKeystore(certificate, pk, password, alias);
+        }
+    %>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>JSP Page</title>
     </head>
     <body>
         <h1>Hello World!</h1>
-        <form method="post" action="generateDownloadKeystore.jsp">
-            CSR<input type="text" name="csrInput"><br>
-            PK<input type="text" name="pkInput"><br>
+        <form method="post" action="create.jsp">
+            CSR<input type="text" name="csrInput" required="true"><br>
+            PK<input type="text" name="pkInput" required="true"><br>
+            Password for Keystore<input type="text" name="password" value="changeit" required="true"><br>
+            Private Key Entry Alias Name<input type="text" name="alias"><br>
             <button type="submit">Generate Keystore</button>
         </form>
     </body>
